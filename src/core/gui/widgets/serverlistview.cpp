cc6b1080a830593b97220aef2cdad58ea192283d
Pol Marcet Sardà <polmarcetsarda@gmail.com>
2019-06-25 22:43:46 +0200

    - Use ResizeToContents in fixed width columns on the server list (#3463)

--- src/core/gui/widgets/serverlistview.cpp
+++ src/core/gui/widgets/serverlistview.cpp
@@ -167,23 +167,12 @@ void ServerListView::setupTableColumnWidths()
 	if (!headerState.isEmpty())
 		horizontalHeader()->restoreState(QByteArray::fromBase64(headerState.toUtf8()));
 
-	// Enforce certain settings on columns, regardless of the state loaded from
-	// the config, like fixed widths, and the visibility.
-	int smallestWidth = horizontalHeader()->minimumSectionSize();
+	// Enforce certain settings on columns, regardless of the state loaded from the config.
 	for (int colIdx = 0; colIdx < ServerListColumnId::NUM_SERVERLIST_COLUMNS; ++colIdx)
 	{
 		setColumnHidden(colIdx, columns[colIdx].bHidden);
-		if (!columns[colIdx].bResizable)
-		{
-			if (!columns[colIdx].bHidden && columns[colIdx].width < smallestWidth)
-			{
-				smallestWidth = columns[colIdx].width;
-				horizontalHeader()->setMinimumSectionSize(smallestWidth);
-			}
-			setColumnWidth(colIdx, columns[colIdx].width);
-		}
 		QHeaderView::ResizeMode resizeMode = columns[colIdx].bResizable ?
-			QHeaderView::Interactive : QHeaderView::Fixed;
+			QHeaderView::Interactive : QHeaderView::ResizeToContents;
 		horizontalHeader()->setSectionResizeMode(colIdx, resizeMode);
 	}
 
